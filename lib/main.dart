901753f4ee4a31edfadafef38ff4437a082800b3
Gustavo Murad <gustavomurad@gmail.com>
2021-07-10 15:27:24 -0300

    Change navigation bar and status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,13 @@ import 'package:authentication_app/pages/welcome_page.dart';
 import 'package:authentication_app/routes/routes.dart';
 import 'package:authentication_app/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: Color(0xFF13161F),
+    statusBarColor: Color(0xFF13161F).withOpacity(0.9),
+  ));
   Routes.createRoutes();
   runApp(App());
 }
